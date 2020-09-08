69e1894f246582c3c115174f9288a57d736250cc
Beornus <s344080@oslomet.no>
2020-09-08 14:51:23 +0200

    Ändring på kod och försöker få riktig sistaVarde.

--- src/Oblig1/Oblig1.java
+++ src/Oblig1/Oblig1.java
@@ -7,19 +7,28 @@ public class Oblig1 {
 
     ///// Oppgave 1 //////////////////////////////////////
     public static int maks(int[] a) {
+        if (a.length < 1) throw new java.util.NoSuchElementException("a.length(" + a.length + ") < 1!");
         int sistaIndex = a.length - 1;
         int maxVarde = a[0];
         int sistaVarde = a[sistaIndex];
-        for(int i=1; i < a.length; i++){
-            if(a[i] >= maxVarde){
-                if (i == sistaIndex){
+        for(int i=1; i < a.length; ++i) {
+            if (a[i] >= maxVarde) {
+                if (i == sistaIndex) {
                     a[sistaIndex] = sistaVarde;
-                    if (sistaVarde > maxVarde){
-                        return sistaVarde;
+                    if (sistaVarde > maxVarde) {
+                        maxVarde = sistaVarde;
                     }
                 }
+                else{
+                    maxVarde = a[i];
+                }
             }
-
+            else{
+                a[i-1] = a[i];
+                a[i] = maxVarde;
+            }
+        }
+        return sistaVarde;
     }
 
     public static int ombyttinger(int[] a) {
